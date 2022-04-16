32138fa8c7d159f471877334656a35a124b667a3
Gbubemi Ogbe <ogbegbubemi15@gmail.com>
2022-04-16 17:34:34 +0100

    Complete build v1.0

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,17 @@
-console.log('Hello World!... Script is working!');
+const ratings = document.querySelectorAll('.rating');
+const rated = document.querySelector('.selected-number');
+const submit = document.querySelector('#submit-button');
+const rate = document.querySelector('.container');
+const thankYou = document.querySelector('.thankyou-container');
+
+ratings.forEach((rating) => {
+    rating.addEventListener('click', () => {
+        let selectedRating = rating.value;
+        rated.innerHTML = selectedRating;
+    });
+});
+
+submit.addEventListener('click', () => {
+    rate.style.display = 'none';
+    thankYou.style.display = 'block';
+});
